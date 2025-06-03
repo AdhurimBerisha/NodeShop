090ddd1f8e53c801a3159da75149fe52b23e7702
AdhurimBerisha <adhurimberisha07@gmail.com>
2025-06-03 16:48:29 +0200

    Code Revamp

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
-const express = require("express");
 const path = require("path");
+
+const express = require("express");
 const bodyParser = require("body-parser");
 
-const errorController = require("./controllers/error.js");
+const errorController = require("./controllers/error");
 
 const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", "views");
 
-const adminRoutes = require("./routes/admin.js");
-const shopRoutes = require("./routes/shop.js");
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
@@ -18,6 +19,6 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
-app.use(errorController.get404Page);
+app.use(errorController.get404);
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -2,10 +2,7 @@ const path = require("path");
 
 const express = require("express");
 
-const productsController = require("../controllers/products.js");
-
-const rootDir = require("../util/path");
-const adminData = require("./admin");
+const productsController = require("../controllers/products");
 
 const router = express.Router();
 
