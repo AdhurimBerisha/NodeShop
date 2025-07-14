f5b9b5dbefcf55d3e74fe032112fec67fe89084d
AdhurimBerisha <adhurimberisha07@gmail.com>
2025-07-14 18:20:45 +0200

    Deleting products

--- controllers/admin.js
+++ controllers/admin.js
@@ -20,7 +20,9 @@ exports.postAddProduct = (req, res, next) => {
     description: description,
   })
     .then((result) => {
-      console.log(result);
+      // console.log(result);
+      console.log("Created");
+      res.redirect("/admin/products");
     })
     .catch((err) => {
       console.log(err);
@@ -84,6 +86,13 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteById(prodId);
-  res.redirect("/admin/products");
+  Product.findByPk(prodId)
+    .then((product) => {
+      return product.destroy();
+    })
+    .then((result) => {
+      console.log("DESTROYRED");
+      res.redirect("/admin/products");
+    })
+    .catch((err) => console.log(err));
 };
