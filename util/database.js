67467294cdb763ceb3f95fb050355accfd8ae2a3
AdhurimBerisha <adhurimberisha07@gmail.com>
2025-07-14 17:00:00 +0200

    Sequelize connection

--- util/database.js
+++ util/database.js
@@ -1,10 +1,8 @@
-const mysql = require("mysql2");
+const Sequelize = require("sequelize");
 
-const pool = mysql.createPool({
+const sequelize = new Sequelize("node-complete", "root", "adhurimbe12", {
+  dialect: "mysql",
   host: "localhost",
-  user: "root",
-  database: "node-complete",
-  password: "adhurimbe12",
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
