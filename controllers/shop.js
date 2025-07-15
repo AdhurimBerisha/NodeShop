55c4d53c32999657c710108d499e363ed43da226
AdhurimBerisha <adhurimberisha07@gmail.com>
2025-07-15 12:54:41 +0200

    Deleting Cart Products

--- controllers/shop.js
+++ controllers/shop.js
@@ -58,27 +58,6 @@ exports.getCart = (req, res, next) => {
         .catch((err) => console.log(err));
     })
     .catch((err) => console.log(err));
-  // Cart.getCart((cart) => {
-  //   Product.fetchAll((products) => {
-  //     const cartProducts = [];
-  //     for (products of products) {
-  //       const cartProductData = cart.products.find(
-  //         (prod) => prod.id === products.id
-  //       );
-  //       if (cartProductData) {
-  //         cartProducts.push({
-  //           productData: products,
-  //           qty: cartProductData.qty,
-  //         });
-  //       }
-  //     }
-  //     res.render("shop/cart", {
-  //       path: "/cart",
-  //       pageTitle: "Your Cart",
-  //       products: cartProducts,
-  //     });
-  //   });
-  // });
 };
 
 exports.postCart = (req, res, next) => {
@@ -116,10 +95,19 @@ exports.postCart = (req, res, next) => {
 
 exports.postCartDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findById(prodId, (product) => {
-    Cart.deleteProduct(prodId, product.price);
-    res.redirect("/cart");
-  });
+  req.user
+    .getCart()
+    .then((cart) => {
+      return cart.getProducts({ where: { id: prodId } });
+    })
+    .then((products) => {
+      const product = products[0];
+      return product.cartItem.destroy();
+    })
+    .then((result) => {
+      res.redirect("/cart");
+    })
+    .catch((err) => console.log(err));
 };
 
 exports.getOrders = (req, res, next) => {
