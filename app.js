3eb71717492cadfe6436084d33449972c4b77f66
AdhurimBerisha <adhurimberisha07@gmail.com>
2025-07-15 18:25:00 +0200

    Add To Cart Functionality

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use((req, res, next) => {
   User.findById("68767cdb0d9fae58acca98dd")
     .then((user) => {
-      req.user = user;
+      req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch((err) => console.log(err));
