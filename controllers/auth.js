89ca850b4b313552bccff3ae3b4f6d229beebcb0
AdhurimBerisha <adhurimberisha07@gmail.com>
2025-07-20 16:51:22 +0200

    Sign up page

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,6 +8,14 @@ exports.getLogin = (req, res, next) => {
   });
 };
 
+exports.getSignup = (req, res, next) => {
+  res.render("auth/signup", {
+    path: "/signup",
+    pageTitle: "Signup",
+    isAuthenticated: false,
+  });
+};
+
 exports.postLogin = (req, res, next) => {
   User.findById("6877caee945a13b586834a02")
     .then((user) => {
@@ -20,6 +28,8 @@ exports.postLogin = (req, res, next) => {
     })
     .catch((err) => console.log(err));
 };
+
+exports.postSignup = (req, res, next) => {};
 
 exports.postLogout = (req, res, next) => {
   req.session.destroy((err) => {
