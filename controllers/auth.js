87ee33a28999e27e4c64136ac59a6e1a848d5006
AdhurimBerisha <adhurimberisha07@gmail.com>
2025-07-20 23:13:59 +0200

    Logic to Update the Password

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,7 @@ const nodemailer = require("nodemailer");
 const bcrypt = require("bcryptjs");
 
 const User = require("../models/user");
+const { hasSubscribers } = require("diagnostics_channel");
 
 const transporter = nodemailer.createTransport({
   host: "sandbox.smtp.mailtrap.io",
@@ -203,6 +204,35 @@ exports.getNewPassword = (req, res, next) => {
         userId: user._id.toString(),
         passwordToken: token,
       });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+exports.postNewPassword = (req, res, next) => {
+  const newPassword = req.body.password;
+  const userId = req.body.userId;
+  const passwordToken = req.body.passwordToken;
+  let resetUser;
+
+  User.findOne({
+    resetToken: passwordToken,
+    resetTokenExpiration: { $gt: Date.now() },
+    _id: userId,
+  })
+    .then((user) => {
+      resetUser = user;
+      return bcrypt.hash(newPassword, 12);
+    })
+    .then((hashedPassword) => {
+      resetUser.password = hashedPassword;
+      resetUser.resetToken = undefined;
+      resetUser.resetTokenExpiration = undefined;
+      return resetUser.save();
+    })
+    .then((result) => {
+      res.redirect("/login");
     })
     .catch((err) => {
       console.log(err);
